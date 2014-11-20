e599476dd2681c84a3725de2765824f50e8abfdc
Antonio <delvecad@gmail.com>
2014-11-20 15:49:06 -0500

    second commit

--- locations.js
+++ locations.js
@@ -1,7 +1,15 @@
+var locationList = [];
+
+//location constructor
 function location(name, description){
 	this.name = name;
 	this.description = description;
 	this.items = [];
 }
 
-var location = new location("mysterious room", "dark");
+//first three locations and descriptions
+var darkRoom = new location("dark room", "dark");
+var fratHouse = new location("Frat House", "still dark");
+var groundhogHole = new location("Groundhog Hole", "even darker");
+
+locationList.push(darkRoom, fratHouse, groundhogHole);
