885bad4caf77bc2be07bd32d4ee1818b37ee86c2
Antonio <delvecad@gmail.com>
2014-11-20 15:56:34 -0500

    third commit

--- locations.js
+++ locations.js
@@ -12,4 +12,19 @@ var darkRoom = new location("dark room", "dark");
 var fratHouse = new location("Frat House", "still dark");
 var groundhogHole = new location("Groundhog Hole", "even darker");
 
+//pushes locations to the locationList array
 locationList.push(darkRoom, fratHouse, groundhogHole);
+
+//adjacency matrix
+var connectionList = [
+	//0 1 2
+     [0,1,0],
+     [0,0,1],
+     [0,1,0]
+];
+
+var map = {
+	locations : locationList,
+	connections : connectionList
+};
+
