791709e42b4964f1c8f1eaf6f8a18439bf6733a9
Antonio <delvecad@gmail.com>
2014-12-13 14:48:01 -0500

    Added items

--- items.js
+++ items.js
@@ -13,3 +13,17 @@ Item.prototype.use = function() {
 Item.prototype.usable = function() {
     return this.uses > 0;
 }
+
+//strong weaons
+var sword = new Item("sword", 999, 10);
+
+//starting weapons
+var crowbar = new Item("crowbar", 999, 2);
+
+//miscellaneous items
+var cup = new Item("cup", 999, 2);
+var groundhogFood = new Item("groundhog food", 3, 5);
+
+//healing items
+var potion = new Item("potion", 1, 10);
+var maxPotion = new Item("max potion", 1, 30 - player.health);
